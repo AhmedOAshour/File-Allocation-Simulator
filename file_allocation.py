1fd210fcd02f8d0d6d603d69a12682082925a13d
Ahmed Ossama <memorykart1@gmail.com>
2020-11-14 13:00:53 +0200

    Console Version 1

---
+++ file_allocation.py
@@ -0,0 +1,66 @@
+class File:
+
+    def __init__(self, size, name):
+        self.size = size
+        self.name = name
+        self.start = None
+
+    def display(self):
+        print("Name: ", self.name, " Size: ", self.size, "Start: ", self.start)
+
+
+class Block:
+
+    def __init__(self, num):
+        self.num = num
+        self.allocated = 0
+        self.blocks = []
+        for i in range(num):
+            self.blocks.append(None)
+
+    def unallocated(self):
+        return self.num-self.allocated
+
+    def display(self):
+        count = 0
+        for i in range(self.num):
+            if self.blocks[i] is None:
+                print(i, " ", self.blocks[i])
+            else:
+                print(i, " ", self.blocks[i].name)
+
+
+class Simulator:
+
+    def __init__(self, files, block_num):
+        self.files = files
+        self.block_num = block_num
+        self.block = Block(block_num)
+
+    def contiguous(self):
+        for file in self.files:
+            start = 0
+            if self.block.unallocated() >= file.size:
+                while True:
+                    count = 0
+                    for i in range(start, start+file.size):
+                        if self.block.blocks[i] is None:
+                            count += 1
+                        else:
+                            start += self.block.blocks[i].size + count + 1
+                            break
+                    if count == file.size:
+                        for i in range(start, file.size):
+                            self.block.blocks[i] = file
+                            self.block.allocated+=1
+                        file.start = start
+                        break;
+                        # allocate
+            else:
+                print(file.name, "couldnt be allocated. Insufficient Space")
+
+    def linked(self):
+        return NotImplementedError()
+
+    def indexed(self):
+        return NotImplementedError()
---
+++ main.py
@@ -0,0 +1,36 @@
+from file_allocation import *
+
+
+# Console Run
+def cmd_run():
+    # block Input
+    block_num = int(input("Enter the number of blocks: "))
+    # file input
+    files = []
+    print("Press Enter when done.")
+    while True:
+        file_name = input("Enter file name: ")
+        if file_name == "":
+            break
+        while True:
+            file_size = input("Enter file size in blocks: ")
+            if not file_size.isnumeric():
+                print("Invalid input. File size must be an integer")
+                continue
+            break
+        # append new file object to array
+        files.append(File(int(file_size), file_name))
+        print("File added.")
+    simulator = Simulator(files, block_num)
+    simulator.contiguous()
+    print("Blocks: ")
+    simulator.block.display()
+    print("Files:")
+    for i in files:
+        i.display()
+
+
+cmd_run()
+
+
+
